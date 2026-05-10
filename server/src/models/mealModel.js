5df2140326dd8104ff32adb56f72c524a744a87c
max-sellick <maxsellick@gmail.com>
2026-05-10 12:15:58 +0200

    updated meal schema

--- server/src/models/mealModel.js
+++ server/src/models/mealModel.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose"
 
 const mealSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     tags: [String],
+    createdBy: { type: Number, ref: 'User', required: true },
     ingredients: [{
         fdcId: Number,
         name: String,
-        amount: Number,
+        amount: Number, // grams
         per100g: {
             calories: Number,
             protein: Number,
@@ -15,7 +16,15 @@ const mealSchema = new mongoose.Schema({
             fiber: Number,
             sugar: Number
         }
-    }]
-})
+    }],
+    totals: { // amounts saved to not calculate every fetch
+        calories: Number,
+        protein: Number,
+        fat: Number,
+        carbs: Number,
+        fiber: Number,
+        sugar: Number
+    }
+}, { timeseries: true })
 
 export default mongoose.model('Meal', mealSchema)
